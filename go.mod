2c873f96ba6b520eb78f3104e6ab837f44594e0d
Kaustav-Mitra <kaustavmitracodes@gmail.com>
2023-06-02 14:29:35 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module learn
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
+}
+
+var books []Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, book := range books {
+		if book.ID == params["id"] {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cotent-Type", "application/json")
+	params := mux.Vars(r)
+	for index, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var updatedBook Book
+			_ = json.NewDecoder(r.Body).Decode(&updatedBook)
+			updatedBook.ID = params["id"]
+			books = append(books, updatedBook)
+			json.NewEncoder(w).Encode(updatedBook)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+
+	books = append(books, Book{ID: "1", Title: "Test Book 1", Author: "Test Author 1", Price: 200})
+	books = append(books, Book{ID: "2", Title: "Test Book 2", Author: "Test Author 2", Price: 400})
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", GetBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", GetBook).Methods("GET")
+	router.HandleFunc("/books", CreateBook).Methods("POST")
+	router.HandleFunc("/books/{id}", UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", DeleteBook).Methods("DELETE")
+
+	fmt.Println("Server Endpoint Hit and Sarted localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
